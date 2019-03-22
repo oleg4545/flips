69f74aef071797996730ee7f667d2482d7f79d45
Oleg <buts.8567@gmail.com>
2019-03-22 21:36:58 +0200

    graphical git commit

--- app/src/main/java/com/firstapp/olegbuts/flips/MainActivity.java
+++ app/src/main/java/com/firstapp/olegbuts/flips/MainActivity.java
@@ -5,12 +5,17 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String oleg;
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-int a = 1;
-int b = 2;
-int c = 3;
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        String name = oleg;
+        int assessment =10;
     }
 }
